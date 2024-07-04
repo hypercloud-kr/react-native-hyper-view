6b397c4a4bf90b53cb764f7749ec67874b8ce9d6
Donald <donald@hyper-cloud.kr>
2024-07-04 14:09:51 +0900

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  HyperViewExample
+//
+
+import Foundation
---
+++ ios/HyperView.swift
@@ -0,0 +1,8 @@
+@objc(HyperView)
+class HyperView: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
