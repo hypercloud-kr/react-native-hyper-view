a361e48a8638657be61ee36c5dba3966461b7e78
Donald <donald@hyper-cloud.kr>
2024-07-05 15:42:00 +0900

    fix: types8

--- android/src/main/java/com/hyperview/HyperViewModule.kt
+++ android/src/main/java/com/hyperview/HyperViewModule.kt
@@ -4,6 +4,7 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
+import com.hypercloud.connect.HyperCloudConnect
 
 class HyperViewModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
@@ -17,6 +18,20 @@ class HyperViewModule(reactContext: ReactApplicationContext) :
   @ReactMethod
   fun multiply(a: Double, b: Double, promise: Promise) {
     promise.resolve(a * b)
+  }
+
+  @ReactMethod
+  fun initializeHyperView() {
+    HyperCloudConnect.initialize(reactContext)
+  }
+
+  @ReactMethod
+  fun openARView(id: Int) {
+    HyperCloudConnect.openUnityActivity(reactContext, id)
+    //        val intent = Intent(reactApplicationContext, TestActivity::class.java)
+    //        intent.putExtra("id", id)
+    //        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+    //        reactApplicationContext.startActivity(intent)
   }
 
   companion object {
