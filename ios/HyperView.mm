e2e798928d3349ddfc5a489285dd832dab3039a0
Donald <donald@hyper-cloud.kr>
2024-07-08 14:40:19 +0900

    fix: types12

--- ios/HyperView.mm
+++ ios/HyperView.mm
@@ -1,7 +1,8 @@
 #import <React/RCTBridgeModule.h>
 
 @interface RCT_EXTERN_MODULE(HyperView, NSObject)
-
+RCT_EXTERN_METHOD(openARView:(NSInteger)nodeId)
+RCT_EXTERN_METHOD(closeARView)
 RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
--- ios/HyperView.swift
+++ ios/HyperView.swift
@@ -1,6 +1,23 @@
+import HyperXRConnect
+
 @objc(HyperView)
 class HyperView: NSObject {
-
+  override init() {
+    super.init()
+    HyperCloudConnect.shared.initialize()
+  }
+  @objc(openARView:)
+  func openARView(withNodeId nodeId:Int){
+    DispatchQueue.main.async {
+      HyperCloudConnect.shared.showUnityView(nodeId: nodeId)
+    }
+  }
+  @objc(closeARView)
+  func closeARView(){
+    DispatchQueue.main.async {
+      HyperCloudConnect.shared.closeUnityView()
+    }
+  }
   @objc(multiply:withB:withResolver:withRejecter:)
   func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
     resolve(a*b)
